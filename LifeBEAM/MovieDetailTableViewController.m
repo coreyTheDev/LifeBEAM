b7ba0f50095964e9b21ffda4300a5a2c8eb80e28
coreyTheDev <coreyzanotti@gmail.com>
2016-10-25 22:06:44 -0400

    populating movie detail information

--- LifeBEAM/MovieDetailTableViewController.m
+++ LifeBEAM/MovieDetailTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "MovieDetailTableViewController.h"
 #import "MovieDetailTableViewCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface MovieDetailTableViewController ()
 
@@ -21,6 +22,8 @@
     [self.tableView registerNib:[UINib nibWithNibName:@"MovieDetailTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:DETAIL_CELL_IDENTIFIER];
     self.tableView.rowHeight = UITableViewAutomaticDimension;
     self.tableView.estimatedRowHeight = 400.0;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.title = self.movie.title;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -41,54 +44,13 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     MovieDetailTableViewCell *detailCell = [tableView dequeueReusableCellWithIdentifier:DETAIL_CELL_IDENTIFIER forIndexPath:indexPath];
-    
-    // Configure the cell...
+    detailCell.overviewLabel.text = self.movie.overview;
+    [detailCell.posterImageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://image.tmdb.org/t/p/w342/%@",self.movie.posterPath]]];
     
     return detailCell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+-(BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
